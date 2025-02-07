f3bcffa0445c9a60c5ccad0da97213c6803105bc
Jeremy Brown <chase208@4JLT.com>
2025-02-07 09:22:17 -0500

    Adding the cert back in

--- modules/certs/main.tf
+++ modules/certs/main.tf
@@ -11,23 +11,23 @@ terraform {
 #   validation_method = "DNS"
 # }
 
-# resource "aws_acm_certificate" "dev_cert" {
-#   domain_name       = var.dev_cert_domain
-#   validation_method = "DNS"
-# }
+resource "aws_acm_certificate" "dev_cert" {
+  domain_name       = var.dev_cert_domain
+  validation_method = "DNS"
+}
 
-# resource "cloudflare_dns_record" "dev_domain_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.dev_cert.domain_validation_options : dvo.domain_name => {
-#       name  = dvo.resource_record_name
-#       value = dvo.resource_record_value
-#       type  = dvo.resource_record_type
-#     }
-#   }
+resource "cloudflare_dns_record" "dev_domain_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.dev_cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      value = dvo.resource_record_value
+      type  = dvo.resource_record_type
+    }
+  }
 
-#   zone_id = var.zone_id
-#   name    = each.value.name
-#   content = each.value.value
-#   type    = each.value.type
-#   ttl     = 300
-# }
+  zone_id = var.zone_id
+  name    = each.value.name
+  content = each.value.value
+  type    = each.value.type
+  ttl     = 300
+}
